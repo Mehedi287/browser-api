3266baea237f08311fc47dfacc993d5c6245af32
Mehedi390 <mh0816150@gmail.com>
2021-09-04 09:41:28 -0700

    frist commit

---
+++ alrt.js
@@ -0,0 +1,15 @@
+const gotoPinice = () => {
+    const respons = confirm('goto to pinice!!!');
+    if (respons === true) {
+        alert('give your fee');
+    }
+    else {
+        alert('get out plase');
+    }
+}
+const aks = () => {
+    const address = prompt('write your address');
+    if (prompt) {
+        console.log(address);
+    }
+}
