b62bf35675b44d5c5a1bc6146ef79e670e450ad0
Mehedi390 <mh0816150@gmail.com>
2021-09-05 06:18:51 -0700

    display cart from localstorage

--- shopping.js
+++ shopping.js
@@ -1,3 +1,10 @@
+const displayLocalStorageCart = () => {
+    const carts = getCart();
+    for (const name in carts) {
+        displayItem(name)
+    }
+}
+
 const addCart = () => {
     const nameFlid = document.getElementById('proudct-name');
     const name = nameFlid.value;
@@ -12,7 +19,7 @@ const displayItem = (name) => {
     const ui = document.getElementById('ul');
     const li = document.createElement('li');
     li.innerText = name;
-    ui.appendChild(li)
+    ui.appendChild(li);
 }
 const getCart = () => {
     const cart = localStorage.getItem('cart');
@@ -27,8 +34,18 @@ const getCart = () => {
 }
 const addItemToCart = (name) => {
     const cart = getCart();
-    cart[name] = 1;
+    if (cart[name]) {
+        cart[name] = cart[name] + 1;
+    }
+    else {
+        cart[name] = 1;
+    }
     console.log(cart);
     const stringify = JSON.stringify(cart);
     localStorage.setItem('cart', stringify)
 }
+const purses = () => {
+    document.getElementById('ul').textContent = '';
+    localStorage.removeItem('cart')
+}
+displayLocalStorageCart();
